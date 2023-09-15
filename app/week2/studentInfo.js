b302c9d29e4261f2104eb3586d7e99e668312c8c
Kiydo <119892780+Kiydo@users.noreply.github.com>
2023-09-14 19:38:33 -0600

    first

---
+++ app/week2/studentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+
+export default function StudentInfo() {
+    return (
+    <main>
+        <p>Name: Edward Magtoto</p>
+        <p>Course: CPRG 306 B</p>
+        <Link href="https://github.com/Kiydo">https://github.com/Kiydo</Link>
+    </main>
+    );
+}
