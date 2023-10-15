a0bd999c4e1fe0cfb1163bf4b307c3f51b723d2f
Kiydo <e.magtoto@hotmail.com>
2023-10-15 15:38:21 -0600

    week5fix

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -5,10 +5,15 @@ import data from "./items.json";
 import { useState } from "react";
 
 export default function ItemList () {
+    
+    const defaultButton = "w-28 py-2 px-4 bg-gray-800 hover:bg-gray-500 rounded-md text-white m-5";
+    const clickedButton = "w-28 py-2 px-4 bg-gray-500 rounded-md text-white m-5";
+
     const [sortBy, setSortBy] = useState("name");
     
     if (sortBy === "name") {
         data.sort((a, b) => a.name.localeCompare(b.name));
+            
     };
     if (sortBy === "category") {
         data.sort((a, b) => a.category.localeCompare(b.category));
@@ -25,11 +30,13 @@ export default function ItemList () {
 
     return (
         <main>
-            <div class="w-28 py-2 px-4 bg-gray-800 hover:bg-gray-500 rounded-md text-white">
-                <button onClick={handleSortName}>Sort by Name</button>
-            </div>
-            <div class="w-28 py-2 px-4 bg-gray-800 hover:bg-gray-500 rounded-md text-white">
-                <button onClick={handleSortCategory}>Sort by Category</button>
+            <div class="inline-flex">
+                <div className={sortBy === "name" ? clickedButton : defaultButton}>
+                    <button id="btnName" onClick={handleSortName}>Sort by Name</button>
+                </div>
+                <div className={sortBy === "category" ? clickedButton : defaultButton}>
+                    <button id="btnCat" onClick={handleSortCategory}>Sort by Category</button>
+                </div>
             </div>
                 {data.map((item) => (
                     <Item name={item.name} quantity={item.quantity} category={item.category} />
--- app/week5/item.js
+++ app/week5/item.js
@@ -2,11 +2,17 @@
 export default function Item ({name, quantity, category}) {
 
     return ( 
-        <div>
+        <div class="flex-wrap w-64 py-2 px-4 bg-red-300 rounded-md m-5">
            <ul>
-                <li>{name}</li>
-                <li>{quantity}</li>
-                <li>{category}</li>
+                <div>
+                    <li>{name}</li>
+                </div>
+                <div>
+                    <li>{quantity}</li>
+                </div>
+                <div>
+                    <li>{category}</li>
+                </div>
            </ul>
         </div>
     );
